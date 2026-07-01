1893dd721585e9f78744d65a2eeadb26e49e9e2a
J Lokeshprabu . <132331354+lokeshtheprogrammer@users.noreply.github.com>
2026-07-01 10:42:53 +0530

    Time: 0 ms (100%), Space: 12.6 MB (37.34%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        for char in s:
+            if char in pairs:
+                if not stack or stack[-1] != pairs[char]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return not stack
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
